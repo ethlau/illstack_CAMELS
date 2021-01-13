856d7976bb4b8ff2d861610e1020dc891eb83299
emilymmoser <42976886+emilymmoser@users.noreply.github.com>
2021-01-13 18:27:14 -0500

    Update generate.py
    Multidimensional vals, all profiles at once

--- CAMELS_example/generate.py
+++ CAMELS_example/generate.py
@@ -2,10 +2,13 @@ import numpy as np
 import subprocess
 import time
 
-sim='IllustrisTNG' #SIMBA
+sim='IllustrisTNG' #IllustrisTNG
                           
-prof2='gasdens'
-prof3='gaspth'
+prof1='gasdens'
+prof2='gaspth'
+prof3='metals_gmw' #gas mass-weighted
+prof4='gasmass'
+prof5='gastemp_gmw'
 
 red_dict={'000':6.0,'001':5.0,'002':4.0,'003':3.5,'004':3.0,'005':2.81329,'006':2.63529,'007':2.46560,'008':2.30383,'009':2.14961,'010':2.00259,'011':1.86243,'012':1.72882,'013':1.60144,'014':1.48001,'015':1.36424,'016':1.25388,'017':1.14868,'018':1.04838,'019':0.95276,'020':0.86161,'021':0.77471,'022':0.69187,'023':0.61290,'024':0.53761,'025':0.46584,'026':0.39741,'027':0.33218,'028':0.27,'029':0.21072,'030':0.15420,'031':0.10033,'032':0.04896,'033':0.0}
 
@@ -42,8 +45,7 @@ for j in simulations:
     for k in snap:
         g='getprof_temp_profiles.sh'
         print('#!/bin/bash',file=open(g,'w'))   
-        print('python', 'profiles_expand.py',prof2,k,sim,j,file=open(g,'a'))
-        #print('python', 'profiles_expand.py',prof3,k,sim,j,file=open(g,'a'))
+        print('python', 'profiles_expand.py',prof1,prof2,prof3,prof4,prof5,k,sim,j,file=open(g,'a'))
         start=time.time()
         subprocess.call(['./getprof_temp_profiles.sh'],shell=True)
         

5747e3660c49bcc20f845402afcf5671501495dc
emilymmoser <42976886+emilymmoser@users.noreply.github.com>
2021-01-13 18:25:14 -0500

    Update istk_params.py
    Multidimensional vals, all the profiles at once

--- CAMELS_example/istk_params.py
+++ CAMELS_example/istk_params.py
@@ -1,11 +1,10 @@
 basepath = '/home/jovyan/Simulations/IllustrisTNG/1P_22/'
 serial      = True
-search_radius = 20
+search_radius = 5 #20 for full, 5 for testing
 lims        = [10e-2,10e4]  #[0.01,10] scaled
 bins        = 25
-mass_low    = 10**10.0  #10^10/h
-mass_high   = 10**15.0  #10^15/h 
-volweight   = True #To convert mass to density
+mass_low    = 10**10.0  #actually 10^10/h
+mass_high   = 10**15.0  #actually 10^15/h 
 scaled_radius = False #True == scaled, False == unscaled
 mass_kind   = 'halo' #options='stellar','halo' 
 save_direct = '/home/jovyan/home/illstack/CAMELS_example/NPZ_files/' 

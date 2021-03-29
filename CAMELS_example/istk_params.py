d0f08bb720d0259ca788247b642173a1321d16f6
emilymmoser <42976886+emilymmoser@users.noreply.github.com>
2021-03-29 15:25:08 -0400

    Update istk_params.py
    update lower mass, save_direct

--- CAMELS_example/istk_params.py
+++ CAMELS_example/istk_params.py
@@ -3,8 +3,8 @@ serial      = True
 search_radius = 6
 lims        = [10e-2,10e4]  #[0.01,10] scaled
 bins        = 25
-mass_low    = 10**10.0  #actually 10^10/h
+mass_low    = 10**11.0  #actually 10^10/h
 mass_high   = 10**15.0  #actually 10^15/h 
 scaled_radius = False #True == scaled, False == unscaled
 mass_kind   = 'halo' #options='stellar','halo' 
-save_direct = '/home/jovyan/home/illstack/CAMELS_example/NPZ_files/' 
+save_direct = '/home/jovyan/home/illstack/CAMELS_example/Batch_NPZ_files/' 
